205f884baa8f3cc0ea8cfdc6fd698488933ae450
Agongar26 <agongar2601@g.educaand.es>
2024-10-18 17:28:48 +0200

    Pagina Terminada v1.0

--- funciones.js
+++ funciones.js
@@ -15,14 +15,17 @@ function loadDoc() {
 }
 
 function ActualizarDoc(Valor) {
-    if (Valor > 0 && Num < 9){
+    if (Valor == 1 && Num < 9){
         Num++;
+        loadDoc();
     } else if (Valor < 0 && Num > 0){
         Num--;
+        loadDoc();
+    } else if (Valor == 2){
+        Num++;
+        Terminar();
     }
-    loadDoc();
 }
-
 
 function myFunction(xml) {
     var xmlDoc = xml.responseXML;
@@ -31,42 +34,54 @@ function myFunction(xml) {
 
     table += "<tr><th>" +
     x[Num].getElementsByTagName("ENCABEZADO")[0].childNodes[0].nodeValue +
-    "</th></tr><tr><td><input type='radio' name='Exam' value='" +
-    x[Num].getElementsByTagName("RESPUESTA")[0].childNodes[0].nodeValue +
-    "' onclick='checkAnswer(this, " + Num + ")'>" +
-    x[Num].getElementsByTagName("RESPUESTA")[0].childNodes[0].nodeValue +
-    "</input></td></tr><tr><td><tr><td><input type='radio' name='Exam' value='"+
-    x[Num].getElementsByTagName("RESPUESTA")[1].childNodes[0].nodeValue +
-    "' onclick='checkAnswer(this, " + Num + ")'>" +
-    x[Num].getElementsByTagName("RESPUESTA")[1].childNodes[0].nodeValue +
-    "</input></td></tr><tr><td><input type='radio' name='Exam' value ='>" +
-    x[Num].getElementsByTagName("RESPUESTA")[2].childNodes[0].nodeValue +
-    "' onclick='checkAnswer(this, " + Num + ")'>" +
-    x[Num].getElementsByTagName("RESPUESTA")[2].childNodes[0].nodeValue +
-    "</input></td></tr><tr><td><input type='radio' name='Exam' value='>" +
-    x[Num].getElementsByTagName("RESPUESTA")[3].childNodes[0].nodeValue +
-    "' onclick='checkAnswer(this, " + Num + ")'>" +
-    x[Num].getElementsByTagName("RESPUESTA")[3].childNodes[0].nodeValue +
-    "</input></td></tr>";
+    "</th></tr>";
 
-    document.getElementById("demo").innerHTML = table + "<br><p id='Calificacion'>Calificación: " + Calificacion + "</p>";
+    for (var i = 0; i < 4; i++) {
+        var respuesta = x[Num].getElementsByTagName("RESPUESTA")[i].childNodes[0].nodeValue;
+        var checked = RespuestasUsuairo[Num] === respuesta ? "checked" : "";
+        table += "<tr><td><input type='radio' name='Exam' value='" +
+        respuesta + "' " + checked + " onclick='checkAnswer(this, " + Num + ")'>" +
+        respuesta + "</input></td></tr>";
+    }
+
+    document.getElementById("demo").innerHTML = table;// + "<br><p id='Calificacion'>Calificación: " + Calificacion + "</p>";
 
     const correctAnswers = xmlDoc.querySelectorAll('RESPUESTA[correcta="true"]');
     RespuestasCorrectas[Num] = correctAnswers[Num].textContent;
 
-    /*let Seleccionada = xmlDoc.querySelector("input[name='RESPUESTA']:checked");
-    RespuestasUsuairo[Num] = Seleccionada.value;*/
-
     //document.getElementById('prueba2').innerHTML = RespuestasCorrectas[Num];
+
+    if(Num<9){
+        document.getElementById("controls").innerHTML = "<button type='button' onclick='ActualizarDoc(-1)'>Anterior</button><button type='button' onclick='ActualizarDoc(1)'>Siguiente</button>";//<p id='prueba'></p><p>" + Num + "</p>"
+    } else{
+        document.getElementById("controls").innerHTML = "<button type='button' onclick='ActualizarDoc(-1)'>Anterior</button><button type='button' onclick='ActualizarDoc(1)'>Siguiente</button></p><button type='button' onclick='ActualizarDoc(2)'>Finalizar</button>";//<p id='prueba'></p><p>" + Num + "</p></p><button type='button' onclick='ActualizarDoc(2)'>Finalizar</button>"
+    } 
 }
 
 function checkAnswer(radio, num) {
+    // Si ya había una respuesta seleccionada, ajustar la calificación
+    if (RespuestasUsuairo[num]) {
+        if (RespuestasUsuairo[num] === RespuestasCorrectas[num]) {
+            Calificacion--;
+        } else {
+            Calificacion += 1/3;
+        }
+    }
+
+    // Actualizar la respuesta seleccionada
+    RespuestasUsuairo[num] = radio.value;
+
+    // Ajustar la calificación según la nueva respuesta
     if (radio.value === RespuestasCorrectas[num]) {
-        //alert("Acierto");
         Calificacion++;
     } else {
-        //alert("Fallo");
         Calificacion -= 1/3;
     }
-    document.getElementById("Calificacion").innerHTML = "Calificación: " + Calificacion;
+
+    //document.getElementById("Calificacion").innerHTML = "Calificación: " + Calificacion;
+}
+
+function Terminar() {
+    document.getElementById("demo").innerHTML = "<p>Tu calificación es: " + Calificacion.toFixed(2) + "/10</p>";
+    document.getElementById("controls").innerHTML = "";
 }
